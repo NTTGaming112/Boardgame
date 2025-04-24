df69954dae516cd86f42e28b9bbb17272e441f66
NTTGaming <nguyentuan02156@gmail.com>
2025-04-24 19:34:58 +0700

    update bg

--- src/features/ataxx/components/BoardSelector.tsx
+++ src/features/ataxx/components/BoardSelector.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/ui/button";
 import { layouts } from "../maps";
+import map1 from "@/assets/map_bg/map1_bg.png"
+import map2 from "@/assets/map_bg/map2_bg.png"
 
 interface BoardSelectorProps {
   selectedBoard: number | null;
   onBoardSelect: (id: number) => void;
 }
+
+const maps: Record<number, string> = {
+  1: map1,
+  2: map2,
+};
 
 const BoardSelector: React.FC<BoardSelectorProps> = ({
   selectedBoard,
@@ -17,6 +24,7 @@ const BoardSelector: React.FC<BoardSelectorProps> = ({
       <h2 className="text-center font-semibold mb-2">Select Board</h2>
       <div className="flex flex-row justify-around">
         {layoutIds.map((id) => (
+          
           <Button
             key={id}
             variant={selectedBoard === id ? "default" : "outline"}
@@ -26,7 +34,7 @@ const BoardSelector: React.FC<BoardSelectorProps> = ({
                 : "border-0 opacity-50"
             } border-2 border-white bg-cover`}
             style={{
-              backgroundImage: `url(/map_bg/map${id}_bg.png)`,
+              backgroundImage: `url(${maps[id]})`,
               backgroundSize: "cover",
               backgroundPosition: "center",
             }}
